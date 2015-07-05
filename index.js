9a33f88d759ea529367f12d0e329fe4186e80181
Will Cullen <will.tw.cullen@gmail.com>
2015-07-05 01:16:11 -0400

    attempt to add sql

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.listen(app.get("port"), function() {
 app.get("/db", function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query("select * users;", function(err, result) {
+      response.send("asdfdf");
       done();
       if (err) {
         console.error(err); response.send("Error " + err);
