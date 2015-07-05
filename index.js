b40162d0dc355f77f95d719d8e082509bcc26a0d
Will Cullen <will.tw.cullen@gmail.com>
2015-07-05 15:29:20 -0400

    attempt to add sql

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var app = express();
 var anyDB = require('any-db')
 var cool = require("cool-ascii-faces");
 
+var MAX_NAME_LENGTH = 16;
+
+
 app.use(express.static(__dirname + "/public"));
 
 app.set("port", (process.env.PORT || 5000));
@@ -18,13 +21,15 @@ var pool = anyDB.createPool(process.env.DATABASE_URL, {
     max: 10 // Maximum connections
 });
 
-
+function isStringBlank(str){
+    return str === null || str.match(/^\s*$/) !== null;
+}
 
 app.get("/db", function (request, response) {
   pool.query("select * from users;", function(err, result) {
     if (err) {
-      console.error(err); response.send("Error " + err);
-      response.send("error");
+      console.error(err);
+      response.json({error: "sql error. oops"});
     } else {
       response.send(result.rows[0].name);
     }
@@ -32,10 +37,75 @@ app.get("/db", function (request, response) {
 });
 
 app.get("/set_name", function(request, response) {
-  request.query.peb_account_token
-  request.query.id
-  request.query.name
+  if (request.query.id != null && isStringBlank(request.query.name)) {
+    response.json({error: "Improper query string"});
+    return;
+  }
 
+  var newName = request.query.name.replace(/[^A-Za-z0-9 ]/g, "");
+  if (newName.length > MAX_NAME_LENGTH) {
+    response.json({error: "Name must be less than 16 characters"});
+    return;
+  }
 
-  response.send(cool());
+  var account_token = "";
+  if (!isStringBlank(request.query.account_token)) {
+    account_token = request.query.account_token;
+  }
+
+  var name_in_use_by_id = 0;
+  pool.query("select id from users where name='" + newName + "';",
+             function(err, result) {
+    if (err) {
+      console.error(err);
+      response.json({error: "sql error. oops"});
+      return;
+    } else if (result.rows.length > 0) {
+      name_in_use_by_id = result.rows[0].id;
+    }
+
+    pool.query("select * from users where (account_token<>'' and account_token='" + account_token + 
+               "') or id=" + request.query.id +");",
+               function(err, result) {
+      if (err) {
+        console.error(err);
+        response.json({error: "sql error. oops"});
+      } else if (result.rows.length > 0 && (name_in_use_by_id == 0 || name_in_use_by_id == result.rows[0].id)) {
+        // change name
+        if (isStringBlank(account_token)) {
+          account_token = result.rows[0].account_token;
+        }
+        pool.query("update users set name='" + newName + "', account_token='" + account_token + 
+                   "' where id=" + result.rows[0].id + " returning *;", function(err, updateResult) {
+          if (err) {
+            console.error(err);
+            response.json({error: "sql error. oops"});
+          } else {
+            response.json({
+              id: updateResult.rows[0].id,
+              name: updateResult.rows[0].name,
+              account_token: updateResult.rows[0].account_token
+            });
+          }
+        });
+      } else if (result.rows.length == 0 && name_in_use_by_id == 0) {
+        // create new user
+        pool.query("insert into users (name, account_token) values ('" + 
+                    newName + "', '" + account_token + "') returning *;", function(err, insertResult) {
+          if (err) {
+            console.error(err);
+            response.json({error: "sql error. oops"});
+          } else {
+            response.json({
+              id: insertResult.rows[0].id,
+              name: insertResult.rows[0].name,
+              account_token: insertResult.rows[0].account_token
+            });
+          }
+        });
+      } else {
+        response.json({error: "This name is already in use. Please choose another name."});
+      }
+    });
+  });
 });
