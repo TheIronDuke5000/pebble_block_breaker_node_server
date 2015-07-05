f15fa6a7419ff12b3e4711144dd5599ad7cdc07d
Will Cullen <will.tw.cullen@gmail.com>
2015-07-05 01:15:05 -0400

    attempt to add sql

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ app.listen(app.get("port"), function() {
 
 app.get("/db", function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query("describe users", function(err, result) {
+    client.query("select * users;", function(err, result) {
       done();
       if (err) {
         console.error(err); response.send("Error " + err);
       } else {
-        response.render("pages/db", {results: result.rows} );
+        response.send("pages/db" + result[0].name );
       }
     });
   });
